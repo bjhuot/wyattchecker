b47caaa8385b697bea448a270752cc658adc5c8b
Brian Huot <brian.huot@gmail.com>
2021-01-27 14:32:19 -0500

    reset to express

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# wyattchecker
-Eligibility Checker for WWDL
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './App.css'
-import Body from './Modules/Body'
-
-function App() {
-  return <Body />
-}
-
-export default App
